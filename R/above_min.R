ebd7f3a20a2d452f9c0864b47f036bf7b0cecace
Robert Emprechtinger <robert.emprechtinger@donau-uni.ac.at>
2022-08-22 17:45:09 +0200

    Fix bug with double exp() for the values of the x axis

--- R/above_min.R
+++ R/above_min.R
@@ -17,8 +17,8 @@ LCL <- 0.8
 min_diff = 1.2
 only_outcome = FALSE
 only_graphdata = FALSE
-lower_better = TRUE
-RR = TRUE
+lower_better = FALSE
+RR = FALSE
 
 above_min <-  function(TE, LCL, min_diff, only_outcome = FALSE, only_graphdata = FALSE, lower_better = TRUE, RR = TRUE){
   # if RR is used get log values
@@ -60,7 +60,7 @@ above_min <-  function(TE, LCL, min_diff, only_outcome = FALSE, only_graphdata =
   # adjustment if we work with RR
   if(RR){
     effect_size <- "RR"
-    #x <- exp(x)
+    x <- exp(x)
     ci_u <- exp(ci_u)
     ci_l <- exp(ci_l)
     min_diff <- exp(min_diff)
@@ -68,65 +68,67 @@ above_min <-  function(TE, LCL, min_diff, only_outcome = FALSE, only_graphdata =
     UCL <- exp(UCL)
     TE <- exp(TE)
   } else{
-    reffect_size <- ""
+    effect_size <- ""
   }
 
+  # create plot annotation for effect size and CI
   data_annotation <- paste(
     "Effect: ", round(TE, 2), effect_size,
     "\nLower CI: ", round(LCL, 2), effect_size,
     "\nUpper CI: ", round(UCL, 2), effect_size
   )
 
-    dat <- data.frame(x = exp(x), y = y)
+  # create plot data frame
+  dat <- data.frame(x = x, y = y)
 
-    # if only the data for the graphs needs to be exported
-    if(only_graphdata) return(list(perc_orig, x, y, ci_u, ci_l,
-                                   min_diff))
+  # if only the data for the graphs needs to be exported
+  if(only_graphdata) return(list(perc_orig, x, y, ci_u, ci_l,
+                                 min_diff))
 
-    # if only the percentage value needs to be exported
-    if(only_outcome){
-      return(perc_orig)
-    } else{
-      # export the plot
-      dat %>%
-        ggplot(aes(x = x, y = y)) +
-        geom_line(alpha = 0.3, size = 1) +
-        geom_area(data =dplyr::filter(dat, x >= LCL, x <= UCL), fill = "black", alpha = 0.2) -> p
+  # if only the percentage value needs to be exported
+  if(only_outcome){
+    return(perc_orig)
+  } else{
+    ###########################
+    # Plotting logic
+    dat %>%
+      ggplot(aes(x = x, y = y)) +
+      geom_line(alpha = 0.3, size = 1) +
+      geom_area(data =dplyr::filter(dat, x >= LCL, x <= UCL), fill = "black", alpha = 0.2) -> p
 
-      # change are direction if lower values are better
-      if(lower_better){
-        p <- p +
-          geom_area(data =dplyr::filter(dat, x <= min_diff), fill = "#72b87f", alpha = 1)
-      } else {
-        p <- p +
-          geom_area(data =dplyr::filter(dat, x >= min_diff), fill = "#72b87f", alpha = 1)
-      }
-
-      p +
-        geom_vline(xintercept = c(ci_u, ci_l), color = "red", alpha = 0.5, linetype = 2) +
-        geom_vline(xintercept = min_diff, color = "#1d8a3a", lwd = 1, alpha = 0.5) +
-        geom_vline(xintercept = TE, color = "black", alpha = 0.2) +
-        theme_light() +
-        xlab(effect_size) +
-        scale_x_continuous(breaks = c(min_diff, TE, ci_u, ci_l) %>% round(2)) +
-        ylab("density") +
-        # Percentage above threshold annotation
-        annotate("text", y = max(y) * 1.1, x = qnorm(0.9999, TE, SE), label = paste(perc, "% ", below_or_above, " MCID Threshold", sep = ""),
-                 color = "black", size = 8) +
-        # Data Annotation
-        annotate("text", y = mean(y), x =  qnorm(0.0001, TE, SE), label = data_annotation,
-                 hjust = 0, vjust = -1, color = "black", size = 5.5) +
-        # Min Difference Annotation
-        annotate("text", y = mean(y), x =  qnorm(0.0001, TE, SE),
-                 label =   paste("\nMCID: ", min_diff %>% round(2), sep = ""),
-                 hjust = 0, vjust = 0, color = "#458251", size = 5.5) +
-        theme(panel.grid = element_blank())
+    # change are direction if lower values are better
+    if(lower_better){
+      p <- p +
+        geom_area(data =dplyr::filter(dat, x <= min_diff), fill = "#72b87f", alpha = 1)
+    } else {
+      p <- p +
+        geom_area(data =dplyr::filter(dat, x >= min_diff), fill = "#72b87f", alpha = 1)
     }
+
+    p +
+      geom_vline(xintercept = c(ci_u, ci_l), color = "red", alpha = 0.5, linetype = 2) +
+      geom_vline(xintercept = min_diff, color = "#1d8a3a", lwd = 1, alpha = 0.5) +
+      geom_vline(xintercept = TE, color = "black", alpha = 0.2) +
+      theme_light() +
+      xlab(effect_size) +
+      scale_x_continuous(breaks = c(min_diff, TE, ci_u, ci_l) %>% round(2)) +
+      ylab("density") +
+      # Percentage above threshold annotation
+      ggtitle(paste(perc, "% ", below_or_above, " MCID Threshold", sep = "")) +
+      # Data Annotation
+      annotate("text", y = mean(y), x =  qnorm(0.0001, TE, SE), label = data_annotation,
+               hjust = 0, vjust = -1, color = "black", size = 5.5) +
+      # Min Difference Annotation
+      annotate("text", y = mean(y), x =  qnorm(0.0001, TE, SE),
+               label =   paste("\nMCID: ", min_diff %>% round(2), sep = ""),
+               hjust = 0, vjust = 0, color = "#458251", size = 5.5) +
+      theme(panel.grid = element_blank())
   }
+}
 
 
 
-above_min(2, 0.8, 1.2, RR = TRUE, lower_better = TRUE)
+above_min(2, 0.8, 1.2, RR = TRUE, lower_better = FALSE)
 
 
 # above_min(1.1, 0.1, 0.6, RR = FALSE)
